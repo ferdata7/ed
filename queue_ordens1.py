8003ce874f5aed79ad4df8b383f56957e25113fa
renatompaes <renatompaes@usp.br>
2026-06-13 23:49:53 -0300

    criando a classe Queue
    usa as classes Node e Order já criados em structures

---
+++ queue_ordens1.py
@@ -0,0 +1,35 @@
+# TRABALHO - SIMULADOR DE LIVRO DE OFERTAS
+# MEMBRO 2 - Fila de Entrada (Queue) e Gerador de Ordens
+
+from structures import Node, Order
+
+class Queue:
+
+    def __init__(self):
+        self.inicio  = None
+        self.fim     = None
+        self.tamanho = 0
+
+    #  insere uma ordem no FIM da fila - O(1)
+    # O(1), pois temos o ponteiro direto para o fim. Não precisamos percorrer a lista toda
+    def enqueue(self, order: Order):
+        novo = Node(order)
+        if self.esta_vazia():
+            self.inicio = novo
+            self.fim = novo
+        else:
+            self.fim.next = novo
+            self.fim = novo
+        self.tamanho += 1
+
+    # dequeue: remove e retorna a ordem do INÍCIO - O(1)
+    def dequeue(self):
+        if self.esta_vazia():
+            print("Erro! Fila vazia — nenhuma ordem para processar!")
+            return None
+        removido      = self.inicio
+        self.inicio   = self.inicio.next
+        if self.inicio is None:
+            self.fim  = None
+        self.tamanho -= 1
+        return removido.data
